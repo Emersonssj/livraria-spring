701a75cd287e058047bd1ebb8bf886101b642db4
Emerson de Oliveira <emerson.msj12@gmail.com>
2026-01-05 20:40:01 -0300

    adicionando exceptionhandler

--- src/main/java/com/altislab/livraria/LivrariaApplication.java
+++ src/main/java/com/altislab/livraria/LivrariaApplication.java
@@ -1,7 +1,13 @@
 package com.altislab.livraria;
 
+import com.altislab.livraria.domain.usuario.UserRole;
+import com.altislab.livraria.domain.usuario.Usuario;
+import com.altislab.livraria.repository.UsuarioRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @SpringBootApplication
